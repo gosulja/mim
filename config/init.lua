774f6ba49d9da41921f6abc2be6c527f206e9b65
blinx <111649405+gosulja@users.noreply.github.com>
2025-02-13 20:31:34 +0000

    Add files via upload

--- config/init.lua
+++ config/init.lua
@@ -1,1 +1,131 @@
+vim.cmd [[packadd packer.nvim]]
 
+require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+
+    use 'neovim/nvim-lspconfig'
+    use 'williamboman/mason.nvim'
+    use 'williamboman/mason-lspconfig.nvim'
+
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'L3MON4D3/LuaSnip'
+    use 'saadparwaiz1/cmp_luasnip'
+
+    use {
+	'nvim-telescope/telescope.nvim',
+	requires = {
+	    'nvim-lua/plenary.nvim'
+	}
+    }
+
+    use 'nvim-tree/nvim-tree.lua'
+    use 'lewis6991/gitsigns.nvim'
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'folke/tokyonight.nvim'
+
+    use 'nvim-lualine/lualine.nvim'
+end)
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+
+vim.cmd[[colorscheme tokyonight]]
+
+vim.g.mapleader = " " -- leader is space
+vim.keymap.set('n', '<leader>w', ':w<CR>')
+vim.keymap.set('n', '<leader>q', ':q<CR>')
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
+
+vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>')
+vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>')
+vim.keymap.set('n', '<leader>fb', ':Telescope buffers<CR>')
+
+require('mason').setup()
+require('mason-lspconfig').setup({
+    ensure_installed = {
+        'clangd',
+        'pyright',
+        'lua_ls',
+    }
+})
+
+local lspconfig = require('lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
+
+lspconfig.clangd.setup{
+    capabilities = capabilities,
+}
+
+lspconfig.pyright.setup{
+    capabilities = capabilities,
+}
+
+lspconfig.lua_ls.setup{
+    capabilities = capabilities,
+}
+
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path' },
+    }),
+})
+
+require('nvim-treesitter.configs').setup({
+    ensure_installed = { "c", "python", "lua" },
+    highlight = {
+        enable = true,
+    },
+})
+
+require('lualine').setup({
+    options = {
+        theme = 'tokyonight'
+    }
+})
+
+require('gitsigns').setup()
+require'nvim-tree'.setup {}
